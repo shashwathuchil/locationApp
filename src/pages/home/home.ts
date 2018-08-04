909aa55c464d32a375d2fda56059e3e8e6447c6b
shashwath <shashwathamruthuchil@gmail.com>
2018-08-04 23:31:38 +0530

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
+declare  var google:any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  latitude:number;
+  longitude:number;
+ 
+  @ViewChild('map') mapRef:ElementRef;
+  constructor(public navCtrl: NavController,
+     public geolocation: Geolocation,
+   ) {
+   
+    }
+    
+    ngOnInit() {
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.latitude = resp.coords.latitude
+        this.longitude = resp.coords.longitude
+        this.DisplayMap();
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    }
 
-  constructor(public navCtrl: NavController) {
+    DisplayMap() {
 
-  }
+      const location = new google.maps.LatLng(this.latitude,
+        this.longitude);
 
+      const options = {
+        center:location,
+        zoom:10,
+        streetViewControl:false,
+        mapTypeId:'satellite',
+      };
+
+      const map = new google.maps.Map(this.mapRef.nativeElement,options);
+
+      this.addMarker(location,map);
+    }
+
+    addMarker(position,map) {
+      return new google.maps.Marker({
+        position,
+        map
+      });
+    }
 }
